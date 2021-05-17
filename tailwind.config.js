36e620a870df1f8abe480cd7b4516c785b0b4631
Olohimai Akalumhe <olohimaiakalumhe@gmail.com>
2021-05-17 13:30:16 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,25 @@ module.exports = {
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {}
-  },
+    extend: {},
+ 
   variants: {},
   plugins: [
     require('@tailwindcss/ui'),
-  ]
+  ],
+  colors: {
+
+    'white': '#FFFFFF',
+    'blue-100': '#020E83',
+    'purple-100': '#EAECF4',
+    'gray-100':'#707070',
+    'gray-200': '#1F2438',
+    'gray-300':'#8283A2',
+    'gray-400':'#787389',
+    'gray-500':'#78738933',
+    
+
+},
 }
+};
+
