805004b81b661c180062aa4e3e1bbb730b4e3e8f
Olohimai Akalumhe <olohimaiakalumhe@gmail.com>
2021-08-18 16:21:12 +0100

    wip

--- resources/views/Tenant/welcome/admissionForm/index.blade.php
+++ resources/views/Tenant/welcome/admissionForm/index.blade.php
@@ -3,12 +3,8 @@
 @endsection
 @section('topNav')
 @endsection
-@section('content')
-    <div class="h-screen md:flex md:overflow-hidden overflow-scroll bg-purple-100">
-        @include('Tenant.welcome.partials._sidebar')
-        <div class="flex-1 overflow-auto bg-purple-100 focus:outline-none px-4 py-8" >
-            @include('Tenant.welcome.admissionForm.partials._admissionForm')
-        </div>
-{{--@include('Tenant.partials._notification')--}}
-    </div>
+@section('content') 
+       <div class="max-h-full">
+        @include('Tenant.welcome.admissionForm.partials._admissionForm')
+       </div>
 @endsection
---
+++ resources/views/Tenant/welcome/admissionForm/partials/_signUp.blade.php
@@ -0,0 +1,21 @@
+
+  <div class="relative grid grid-cols-1 gap-6 max-w-md ">
+     <div>
+         <label for="setPassword" class="block text-sm font-medium text-gray-300">Set Password</label>
+         <div class="mt-1">
+         <input type="password" name="setPassword" id="setPassword" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-purple-100 rounded-md p-2 border">
+         </div>
+     </div>
+     <div>
+         <label for="confirmPassword" class="block text-sm font-medium text-gray-300">Confirm Password  </label>
+         <div class="mt-1">
+         <input type="password" name="confirmPassword" id="confirmPassword" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-purple-100 rounded-md p-2 border">
+         </div>
+     </div>
+     <div class="flex justify-bottom-end  pb-52 md:pb-0">
+        <button class="bg-blue-100 text-white px-6 py-2 rounded-md text-sm">
+           Next
+        </button>
+    </div>
+  </div>
+  
